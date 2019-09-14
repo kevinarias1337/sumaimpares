d598fc70b6dfdb31083ecbdd440d0619dee3c897
kevinarias1337 <54506222+kevinarias1337@users.noreply.github.com>
2019-09-14 12:58:19 -0500

    Add files via upload

---
+++ sumaimpares/sumaimpares/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace sumaimpares
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.limitetxt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbltotal = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btncalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label1.Location = new System.Drawing.Point(261, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese el número límite:";
+            // 
+            // limitetxt
+            // 
+            this.limitetxt.Location = new System.Drawing.Point(429, 45);
+            this.limitetxt.Name = "limitetxt";
+            this.limitetxt.Size = new System.Drawing.Size(36, 20);
+            this.limitetxt.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.label2.Location = new System.Drawing.Point(165, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(408, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "La suma de los números impares hasta el número ingresado es de:";
+            // 
+            // lbltotal
+            // 
+            this.lbltotal.AutoSize = true;
+            this.lbltotal.Location = new System.Drawing.Point(579, 92);
+            this.lbltotal.Name = "lbltotal";
+            this.lbltotal.Size = new System.Drawing.Size(13, 13);
+            this.lbltotal.TabIndex = 3;
+            this.lbltotal.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(125, 29);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(517, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "_________________________________________________________________________________" +
+    "____";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(125, 136);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(517, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "_________________________________________________________________________________" +
+    "____";
+            // 
+            // btncalcular
+            // 
+            this.btncalcular.Location = new System.Drawing.Point(471, 45);
+            this.btncalcular.Name = "btncalcular";
+            this.btncalcular.Size = new System.Drawing.Size(44, 20);
+            this.btncalcular.TabIndex = 7;
+            this.btncalcular.Text = "->";
+            this.btncalcular.UseVisualStyleBackColor = true;
+            this.btncalcular.Click += new System.EventHandler(this.Btncalcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 292);
+            this.Controls.Add(this.btncalcular);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lbltotal);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.limitetxt);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Suma de números impares.";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox limitetxt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbltotal;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btncalcular;
+    }
+}
+
---
+++ sumaimpares/sumaimpares/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sumaimpares
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Btncalcular_Click(object sender, EventArgs e)
+        {
+            int limitetxt2 = int.Parse(limitetxt.Text);
+            int i = 1;
+            int x = 0;            
+            for (i=1;i==limitetxt2 ; i++)
+            {
+                do
+                {
+                    x = i + i;                    
+                }
+                while (i % 2 == 0);                
+            }
+            lbltotal.Text = x.ToString();
+        }
+    }
+}
